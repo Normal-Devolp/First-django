d4c95ba289f8691b111ae11ed6e2355bc83307e0
Normal-Devplp <momessi207@gmail.com>
2021-03-25 19:12:01 +0200

    creat app [test1]

--- post/models.py
+++ post/models.py
@@ -17,3 +17,4 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverses("Post_detail", kwargs={"pk": self.pk})
+
--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'post',
+    'test1.app.Test1Config',
 ]
 
 MIDDLEWARE = [
